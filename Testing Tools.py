68e545d7ac7964772a7f8861a8cbe38b8573176e
Rihan Ahmed <95731297+Rihan444@users.noreply.github.com>
2021-12-08 09:30:06 +0600

    Add files via upload

---
+++ Testing Tools.py
@@ -0,0 +1,36 @@
+color_off="\033[0m"       # Text Reset
+red="\033[0;31m"          # Red
+blue="\033[0;34m"         # Blue
+yellow="\033[0;33m"       # Yellow
+
+
+print(red+'''
+$$$$$$$\  $$\ $$\                                 $$$$$$$$\                  $$\           
+$$  __$$\ \__|$$ |                                \__$$  __|                 $$ |          
+$$ |  $$ |$$\ $$$$$$$\   $$$$$$\  $$$$$$$\           $$ | $$$$$$\   $$$$$$\  $$ | $$$$$$$\ 
+$$$$$$$  |$$ |$$  __$$\  \____$$\ $$  __$$\          $$ |$$  __$$\ $$  __$$\ $$ |$$  _____|
+$$  __$$< $$ |$$ |  $$ | $$$$$$$ |$$ |  $$ |         $$ |$$ /  $$ |$$ /  $$ |$$ |\$$$$$$\  
+$$ |  $$ |$$ |$$ |  $$ |$$  __$$ |$$ |  $$ |         $$ |$$ |  $$ |$$ |  $$ |$$ | \____$$\ 
+$$ |  $$ |$$ |$$ |  $$ |\$$$$$$$ |$$ |  $$ |         $$ |\$$$$$$  |\$$$$$$  |$$ |$$$$$$$  |
+\__|  \__|\__|\__|  \__| \_______|\__|  \__|         \__| \______/  \______/ \__|\_______/ 
+                                                                                           
+                                                                                           
+                                                                                           
+'''+color_off)
+
+print(blue+'''1/Facebook Hacking ''')  
+print('''2/Instagram Hacking ''') 
+print('''3/Free Fire Hacking ''')
+print('''4/PUBG Hacking ''')
+print('''5/Wifi Hacking ''')
+print('''6/Sim Hacking ''')
+print('''7/Location Hacking''')
+print('''8/Rihan Spesal Tools ''')
+print('''9/Camera Hacking ''')
+print('''10/Ip Tracking''')
+print('''11/OTP Bypass''')     
+print('''12/Call Bombing''')
+print('''13/SMS Bombing'''+color_off)
+print(yellow+'''\nMy Number +8809638485201          ''')                                                                                                                                                                                                                    
+                                                         
+                                                         
