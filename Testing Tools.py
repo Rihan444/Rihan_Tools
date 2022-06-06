e827be2f509658c2c9ca7e6d6f9ab2156a6cd503
Rihan Ahmed <95731297+Rihan444@users.noreply.github.com>
2022-06-06 06:20:11 +0600

    Update Testing Tools.py

--- Testing Tools.py
+++ Testing Tools.py
@@ -1,10 +1,20 @@
+
+import sys
+
+
 color_off="\033[0m"       # Text Reset
 red="\033[0;31m"          # Red
 blue="\033[0;34m"         # Blue
 yellow="\033[0;33m"       # Yellow
+biblue="\033[1;94m"       # Blue
+bipurple="\033[1;95m"     # Purple
+bicyan="\033[1;96m"       # Cyan
+bgreen="\033[1;32m"       # Green0
+bred="\033[1;31m"         # Red
+on_igreen="\033[0;102m"   # Green
+igreen="\033[0;92m"       # Green
 
-
-print(red+'''
+print(bicyan+'''
 $$$$$$$\  $$\ $$\                                 $$$$$$$$\                  $$\           
 $$  __$$\ \__|$$ |                                \__$$  __|                 $$ |          
 $$ |  $$ |$$\ $$$$$$$\   $$$$$$\  $$$$$$$\           $$ | $$$$$$\   $$$$$$\  $$ | $$$$$$$\ 
@@ -12,11 +22,24 @@ $$$$$$$  |$$ |$$  __$$\  \____$$\ $$  __$$\          $$ |$$  __$$\ $$  __$$\ $$ 
 $$  __$$< $$ |$$ |  $$ | $$$$$$$ |$$ |  $$ |         $$ |$$ /  $$ |$$ /  $$ |$$ |\$$$$$$\  
 $$ |  $$ |$$ |$$ |  $$ |$$  __$$ |$$ |  $$ |         $$ |$$ |  $$ |$$ |  $$ |$$ | \____$$\ 
 $$ |  $$ |$$ |$$ |  $$ |\$$$$$$$ |$$ |  $$ |         $$ |\$$$$$$  |\$$$$$$  |$$ |$$$$$$$  |
-\__|  \__|\__|\__|  \__| \_______|\__|  \__|         \__| \______/  \______/ \__|\_______/ 
-                                                                                           
-                                                                                           
-                                                                                           
-'''+color_off)
+\__|  \__|\__|\__|  \__| \_______|\__|  \__|         \__| \______/  \______/ \__|\_______/ '''+color_off)
+
+print(bgreen+'''CEO- Bangladash Hacking Help Centre     '''                                   +color_off)
+print(bred+'''CEO- Dark 420 Spamming Team     '''                              +color_off)
+print(bgreen+'''Tools Developer- Rihan Ahmed    '''                            +color_off)
+
+usern='''Rihan'''
+passwd='''The'''
+
+inpuser=str(input('''Enter The Username: '''))
+inppass=str(input('''Enter The Password: '''))
+
+if usern==inpuser and passwd==inppass:
+	print(igreen+'''[✓] Username & Password Correct! '''+color_off)
+	pass
+else:
+	print(bred+'''[×] Wrong Username & Password!  '''+color_off)
+	sys.exit()
 
 print(blue+'''1/Facebook Hacking ''')  
 print('''2/Instagram Hacking ''') 
@@ -31,6 +54,5 @@ print('''10/Ip Tracking''')
 print('''11/OTP Bypass''')     
 print('''12/Call Bombing''')
 print('''13/SMS Bombing'''+color_off)
-print(yellow+'''\nMy Number +8809638485201          ''')                                                                                                                                                                                                                    
-                                                         
-                                                         
+print(yellow+'''My Number: +8809638485201              '''+color_off)
+print('''Select Your Option:-''')
